bd200589afdbe8d4723bdb2a24c2cc805fe9df6b
orviz <orviz@ifca.unican.es>
2022-09-30 13:00:09 +0200

    Add JePL file structure

---
+++ .sqa/script.squirrely-cerise-mau.sh
@@ -0,0 +1,4 @@
+(
+cd github.com/I2PC/scipion-docker &&
+    git rev-parse --is-inside-work-tree
+)
---
+++ .sqa/script.wiggy-jade-mau.sh
@@ -0,0 +1,4 @@
+(
+cd github.com/I2PC/scipion-docker &&
+    find_doc_files.py --file_type all
+)
